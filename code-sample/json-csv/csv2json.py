af9618deceb4e86d7337c2211dd3bde19c9bc742
marvel-soft <jimbo.sievers@gmail.com>
2021-05-02 07:31:23 -0700

    minor edits

--- code-sample/json-csv/csv2json.py
+++ code-sample/json-csv/csv2json.py
@@ -1,13 +1,25 @@
 import pandas as pd 
 import numpy as np
 import csv
-
-df = pd.read_csv (r'2020-sd6.csv')
-df.to_json (r'2020-sd6.json')
+import os
 
 row_index = 0
 
-df = pd.read_json (r'2020-sd6.json')
-df.to_csv (r'2020-sd6-new.csv', index = None)
 
-row_index = 0
+def main():
+    print(os.getcwd())
+    '''
+    df = pd.read_csv (r'testdata/2020-sd6.csv')
+    df.to_json (r'2020-sd6.json')
+
+    row_index = 0
+
+    df = pd.read_json (r'testdata/2020-sd6.json')
+    df.to_csv (r'testdata/testdata/2020-sd6-new.csv', index = None)
+    '''
+
+    row_index = 1
+    return 0
+
+if __name__ == '__main__':
+    main()
