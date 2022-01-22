cb88a36f3db88876d7c8f76366a91a216567c6dd
marvel-soft <jimbo.sievers@gmail.com>
2022-01-22 07:29:45 -0800

    add new nvreport.sh for python

--- sos-voter-files/nvreport.sh
+++ sos-voter-files/nvreport.sh
@@ -3,8 +3,7 @@ echo 'Starting i360 Survey Log Analyzer'
 echo '#=======================================================#'
 echo '#     This script analyzers i360 survey logs            #'
 echo '#=======================================================#'
-export my_path=/Users/jimsievers/symlinks/2022-electioneering/canvass/3survey-responses/surveys
 cd ~/Downloads
-echo $my_path
-python ~/python/nvreport.py -infile i360 -datadir ~/symlinks/electioneering/2022-electioneering/canvass/3survey-responses/ -rptfile SurveyResponseRpt
+pwd
+python ~/python/nvreport.py -infile i360 -datadir ~/Dropbox/2022-electioneering/p1-canvass/3survey-responses/ -rptfile SurveyResponseRpt
 echo 'COMPLETED REPORT'
